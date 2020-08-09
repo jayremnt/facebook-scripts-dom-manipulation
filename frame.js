ef2b0d1d52220877146bff507235c3c615182932
Nguyễn Duy Bảo <62267107+JayremntB@users.noreply.github.com>
2020-08-09 15:30:16 +0700

    Update frame.js

--- frame.js
+++ frame.js
@@ -1,19 +1,28 @@
+/*
+* Code by (author)
+* Requirement: (Old/New) Facebook 
+* Please copy all the code to make sure that you will not get any errors
+* Please switch to (OLD/NEW) Facebook and navigate to this link: (link)
+*/
+// You can change the delay time or not (in milliseconds, 1 s = 1000 ms)
+// REMEMBER: the longer the delay time you set, the bigger your chance of not getting Facebook Checkpoint
 let timePerAction = 1000;
 let timeToLoadPage = 3000;
 
 (() => {
-	console.log("Code by JayremntB, 2020");
+	console.log("\x1b[36m%s\x1b[0m", "Code by (author)");
+	console.log("\x1b[36m%s\x1b[0m", "Please remember if you meet an error, just reload page, wait for 3 seconds and run the code again");
 	if(timePerAction < 500) return console.warn("time per action must be set >= 500 ms");
 	// prepare
-	let thingAims;
-	let subThingIndex = 0;
+	let listButtons;
+	let buttonIndex = 0;
 	// process
 	setTimeout(function continuousWhenPageLoad() {
-		thingAims = document.getElementsByClassName('');
-		if(thingAims.length === 0 || subThingIndex > thingAims.length - 1) return console.warn("Not found any ...");
-		thingAims[].click();
-		setTimeout(function handleNextSubThing() {
-			if(subThingIndex > thingAims.length - 1) {
+		listButtons = document.getElementsByClassName('');
+		if(listButtons.length === 0 || buttonIndex > listButtons.length - 1) return console.warn("Not found ???");
+		setTimeout(function clickNextButton() {
+			// if reach the last button, scroll to load more
+			if(buttonIndex > listButtons.length - 1) {
 				// scroll
 				// with things like dialog
 				document.getElementsByClassName('')[0].scrollTo(0, document.getElementsByClassName('')[0].scrollHeight);
@@ -22,15 +31,17 @@ let timeToLoadPage = 3000;
 				setTimeout(continuousWhenPageLoad, timeToLoadPage);
 				return;
 			}
-			// click first thing
-			thingAims[subThingIndex].click();
+			// click button
+			listButtons[buttonIndex].click();
+			// wait for sth, and do sth
 			setTimeout(() => {
-				
+				// do sth here
 			}, timeToDoSth);
 			// get user's name
-			let sthHasName = thingAims[subThingIndex].textContent;
-			console.log(`  ${sthHasName} successfully!`);
-			setTimeout(handleNextSubThing, timePerAction);
+			let sthHasName = listButtons[buttonIndex].textContent;
+			console.log(`(action) ${sthHasName} successfully!`);
+			buttonIndex ++;
+			setTimeout(clickNextButton, timePerAction);
 		}, 0);
-}, 2000);
+	}, 2000);
 })();
