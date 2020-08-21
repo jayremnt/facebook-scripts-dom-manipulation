a0b489d110609b487731460c9d2be6711a1bcdb8
Jayremnt Nguyen <jayremntb@gmail.com>
2020-08-21 23:46:38 +0700

    fix auto skip error (wrong method to get label)

--- add-friends-in-list-group-members-or-in-another-list-friends/new-facebook.js
+++ add-friends-in-list-group-members-or-in-another-list-friends/new-facebook.js
@@ -24,7 +24,7 @@ let totalFriendRequestsSent = 1000; // the number of users you want to send frie
     let AddFriendButtonIndex = 0;
     //
     setTimeout(function continuousWhenPageLoad() {
-      let listUsers = document.getElementsByClassName('bp9cbjyn ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi n1f8r23x rq0escxv j83agx80 bi6gxh9e discj3wi hv4rvrfc ihqw7lf3 dati1w0a gfomwglr');
+      let listUsers = document.getElementsByClassName('oajrlxb2 tdjehn4e gcieejh5 bn081pho humdl8nn izx4hr6d rq0escxv nhd2j8a9 j83agx80 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys d1544ag0 qt6c0cv9 tw6a2znq i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l beltcj47 p86d2i9g aot14ch1 kzx2olss cbu4d94t taijpn5t ni8dbmo4 stjgntxs k4urcfbm tv7at329');
       if(listUsers.length === 0 || AddFriendButtonIndex > listUsers.length - 1) return console.warn("You have reached the end of list friends");
       // click Add Friend buttons
       setTimeout(function clickNextAddFriendButton() {
@@ -38,15 +38,15 @@ let totalFriendRequestsSent = 1000; // the number of users you want to send frie
         // get user's name
         const userName = listUsers[AddFriendButtonIndex].parentNode.parentNode.parentNode.parentNode.previousSibling.firstChild.firstChild.firstChild.textContent
         // in case not Add Friend Button
-        let label = listUsers[AddFriendButtonIndex].lastChild.firstChild.firstChild.firstChild.getAttribute('aria-label');
-        if(!label || (label.toLowerCase() !== 'thêm bạn bè' && label.toLowerCase() !== "add friend")) {
+        let label = listUsers[AddFriendButtonIndex].getAttribute('aria-label');
+        if(!label || (label && (label.toLowerCase() !== 'thêm bạn bè' && label.toLowerCase() !== "add friend"))) {
             console.log(`Skip ${userName}`);
             AddFriendButtonIndex ++;
             setTimeout(clickNextAddFriendButton, 0);
             return;
         }
         // click Add Friend Button
-        listUsers[AddFriendButtonIndex].lastChild.firstChild.firstChild.firstChild.click();
+        listUsers[AddFriendButtonIndex].click();
         console.log(`Sent friend request to ${userName} - ${--totalFriendRequestsSent} remanding...`);
         // click next Add Friend button
         AddFriendButtonIndex ++;
@@ -74,7 +74,7 @@ let totalFriendRequestsSent = 1000; // the number of users you want to send frie
         // get user's name
         const userName = listUsers[AddFriendButtonIndex].firstChild.nextSibling.firstChild.textContent;
         // in case not Add Friend Button
-        let label = listUsers[AddFriendButtonIndex].lastChild.firstChild.getAttribute('aria-label');
+        let label = listUsers[AddFriendButtonIndex].lastChild.firstChild.firstChild.firstChild.getAttribute('aria-label');
         if(!label || (label && (label.toLowerCase() !== 'thêm bạn bè' && label.toLowerCase() !== "add friend"))) {
             console.log(`Skip ${userName}`);
             AddFriendButtonIndex ++;
@@ -82,7 +82,7 @@ let totalFriendRequestsSent = 1000; // the number of users you want to send frie
             return;
         }
         // click Add Friend Button
-        listUsers[AddFriendButtonIndex].lastChild.firstChild.click();
+        listUsers[AddFriendButtonIndex].lastChild.firstChild.firstChild.firstChild.click();
         console.log(`Sent friend request to ${userName} - ${--totalFriendRequestsSent} remanding...`);
         // click next Add Friend button
         AddFriendButtonIndex ++;
